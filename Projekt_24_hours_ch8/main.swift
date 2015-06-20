b8f0f10a1304eb23e8884eb23bfa98ebadf5df12
Nicolas Ducat <nicolas.ducat@gmx.net>
2015-06-20 10:20:05 +0200

    Initial Commit

---
+++ Projekt_24_hours_ch8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projekt_24_hours_ch8
+//
+//  Created by Nicolas on 20.06.15.
+//  Copyright (c) 2015 Peter Pan Enterprises. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
